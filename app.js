beb786e7ed4b9515c47f13e7a144045b8f7a9237
Tyson Lott <jtysonlott@gmail.com>
2021-04-15 21:00:47 -0400

    updates

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ LIST OF FUNCTIONS
 function toggleNav() {
     document.getElementsByClassName("burger")[0].classList.toggle("active");
   }
+
+  function toggleLinks() {
+    document.getElementsByClassName("links")[0].classList.toggle("hidden");
+  }
